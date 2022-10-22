7bafec42eb6c6abff972c490a9f05ae27ecbcd6f
Alessandra Romero Ledesma <00862450152@ifms.edu.br>
2022-10-21 21:57:10 -0400

    initial commit

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HeaderStyle = styled.header`
+    width: 100%;
+    height: 60px;
+    background-color: #6959CD;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    h1{
+      font-size  :22px ;
+    }
+`
